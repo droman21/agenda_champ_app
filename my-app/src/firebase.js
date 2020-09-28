c9ac0a60dd9bdf359dc3bbbc167f6d57a4b79720
Daniel Roman <dan.roman21@gmail.com>
2020-09-28 14:32:17 -0400

    Set up app in Firebase and added api creds to package.json

---
+++ my-app/src/firebase.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+const firebaseConfig = firebase.initializeApp({
+    apiKey: "AIzaSyA7J6S8AcHgEKWG81lmgPX3zurTaYLgJnk",
+    authDomain: "agendachamp-1b33b.firebaseapp.com",
+    databaseURL: "https://agendachamp-1b33b.firebaseio.com",
+    projectId: "agendachamp-1b33b",
+    storageBucket: "agendachamp-1b33b.appspot.com",
+    messagingSenderId: "936058783664",
+    appId: "1:936058783664:web:5530f4eed09bac0c967875",
+    measurementId: "G-VTVGGT7GJD"
+});
+
+export {firebaseConfig as firebase};
