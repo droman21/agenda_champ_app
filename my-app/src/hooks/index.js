6984684f683e35b01d578b6ac8e398bcd198ed57
Daniel Roman <dan.roman21@gmail.com>
2020-09-28 14:59:00 -0400

    Unsubscribe formatting

--- my-app/src/hooks/index.js
+++ my-app/src/hooks/index.js
@@ -1,4 +1,27 @@
 import {useState, useEffect} from 'react';
 import {firebase} from '../firebase';
 
-export const useTasks = selectedProject => 
+const collatedTasks = () => {};
+
+export const useTasks = selectedProject => {
+    const [tasks, setTasks] = useState([]);
+    
+    useEffect(() => {
+        let unsubscribe = firebase
+            .firestore()
+            .collection('tasks')
+            .where('userId', '==', '7dbaf0e0-983c-48f1-a43a-c5f30af5bacd');
+
+        unsubscribe = selectedProject &&  !collatedTasksExist(selectedProject) ?
+        (unsubscribe = unsubscribe.where('projectId', '==', selectedProject))
+        : selectedProject === 'TODAY'
+        ? (unsubscribe = unsubscribe.where(
+            'date',
+            '==', 
+            moment().format('DD/MM/YYYY')
+            ))
+        : selectedProject === 'INBOX' || selectedProject === 0
+        ? (unsubscribe = unsubscribe.where('date', '==', ''))
+        : unsubscribe;
+    }, []);
+};
