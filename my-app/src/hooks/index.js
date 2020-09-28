abcf665a2b61b5e09bfc8ac7460e3e63c1a9bb2f
Daniel Roman <dan.roman21@gmail.com>
2020-09-28 15:34:46 -0400

    Finished unsubscribe hook, starting useProjects hook

--- my-app/src/hooks/index.js
+++ my-app/src/hooks/index.js
@@ -6,6 +6,7 @@ import {collatedTasksExist} from '../helpers';
 
 export const useTasks = selectedProject => {
     const [tasks, setTasks] = useState([]);
+    const [archivedTasks, setArchivedTasks] = useState([]);
     
     useEffect(() => {
         let unsubscribe = firebase
@@ -18,11 +19,36 @@ export const useTasks = selectedProject => {
         : selectedProject === 'TODAY'
         ? (unsubscribe = unsubscribe.where(
             'date',
-            '==', 
+            '==',
             moment().format('DD/MM/YYYY')
             ))
         : selectedProject === 'INBOX' || selectedProject === 0
         ? (unsubscribe = unsubscribe.where('date', '==', ''))
         : unsubscribe;
-    }, []);
+
+      unsubscribe = unsubscribe.onSnapshot(snapshot => {
+        const newTasks = snapshot.docs.map(task => ({
+            id: task.id,
+            ...task.data(),
+        }));
+
+        setTasks(
+            selectedProject === 'NEXT_7'
+            ? newTasks.filter(
+                task =>
+                    moment(task.date, 'DD-MM-YYYY').diff(moment(), 'days') <= 7 &&
+                    task.archived !== true
+                )
+            : newTasks.filter(task => task.archived !== true)
+        );
+
+        setArchivedTasks(newTasks.filter(task => task.archived !== false));
+      });
+
+      return () => unsubscribe();
+    }, [selectedProject]);
+
+    return {tasks, archivedTasks};
 };
+
+export const useProjects = () =>
